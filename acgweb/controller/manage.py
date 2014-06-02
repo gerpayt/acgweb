a3f959df3da4b5cac2da8ec960e240933238d3a8
chenfeng <root@haitou116.(none)>
2014-06-02 22:06:20 +0800

    show salary manage endtime

--- acgweb/controller/manage.py
+++ acgweb/controller/manage.py
@@ -44,14 +44,14 @@ def salarymanage():
     if not endtime: endtime = CONST.max_time
     export = request.args.get('export',None)
 
-    sql1 = "select c.uid, b.id, b.start_time, b.venue, b.title, b.end_time-b.start_time as last_time from duty as a left join activity as b on a.aid = b.id left join member as c on c.uid = a.uid where b.end_time != '0' and b.start_time > '%d' and b.end_time < %d and a.status = '11' order by b.start_time " % (int(starttime), int(endtime))
+    sql1 = "select c.uid, b.id, b.start_time, b.end_time, b.venue, b.title, b.end_time-b.start_time as last_time from duty as a left join activity as b on a.aid = b.id left join member as c on c.uid = a.uid where b.end_time != '0' and b.start_time > '%d' and b.end_time < %d and a.status = '11' order by b.start_time " % (int(starttime), int(endtime))
     #print sql1
     res1 = db.session.execute(sql1)
     salalist = {}
     for i in res1:
         if not salalist.has_key(i.uid):
             salalist[i.uid] = []
-        salalist[i.uid].append({'aid':i.id,'start_time':i.start_time-3600,'venue':i.venue,'title':i.title,'work_last_time':i.last_time+3600})
+        salalist[i.uid].append({'aid':i.id,'start_time':i.start_time-3600,'end_time':i.end_time,'venue':i.venue,'title':i.title,'work_last_time':i.last_time+3600})
     #print salalist
     sql2 = "select c.uid, c.name, c.credit_card, sum(b.end_time-b.start_time+3600) as totaltime from duty as a left join activity as b on a.aid = b.id left join member as c on c.uid = a.uid where b.end_time != '0' and b.start_time > '%d' and b.end_time < %d and a.status = '11' group by a.uid order by totaltime " % (int(starttime), int(endtime))
     #print sql2
