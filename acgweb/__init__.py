202a1a5ad841c101f7c3a1d4605224afa3e86a26
chenfeng <gerpayt@qq.com>
2014-09-23 15:27:31 +0800

    增加错误日志

--- acgweb/__init__.py
+++ acgweb/__init__.py
@@ -1,8 +1,10 @@
-# -- coding : utf-8 --
+# coding: utf-8
 import sys
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from . import config
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
 
 # convert python's encoding to utf8
 reload(sys)
@@ -17,6 +19,11 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://%s:%s@%s/%s' % (
     config.DB_USER, config.DB_PASSWORD, config.DB_HOST, config.DB_NAME)
 app.config['SQLALCHEMY_POOL_RECYCLE'] = 5
 db = SQLAlchemy(app)
+
+file_handler = RotatingFileHandler('./acgweb/log/error.log', 'a', 1 * 1024 * 1024, 10)
+file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+file_handler.setLevel(logging.INFO)
+app.logger.addHandler(file_handler)
 
 import model
 import controller
