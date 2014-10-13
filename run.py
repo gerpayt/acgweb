27ab8de3a64cdff39f1d953697535c3a1802000c
chenfeng <gerpayt@qq.com>
2014-10-13 23:18:50 +0800

    增加性能评估接口

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 # coding: utf-8
+from werkzeug.contrib.profiler import ProfilerMiddleware
 from acgweb import app
 
 #app.run(host='0.0.0.0',debug=True)
 if __name__ == '__main__':
+    if app.config['PROFILE']:
+        app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[30])
     app.run(host='0.0.0.0')
 application = app
