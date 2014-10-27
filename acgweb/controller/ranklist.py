617aaca5c8d1c16524554525acb850a548d0dc4a
chenfeng <gerpayt@qq.com>
2014-10-27 23:23:11 +0800

    统计开始时间区间提示

--- acgweb/controller/ranklist.py
+++ acgweb/controller/ranklist.py
@@ -19,10 +19,10 @@ def ranklist():
     month_start = int(time.mktime((struct_now.tm_year, struct_now.tm_mon, 1, 0, 0, 0, 0, 0, 0)))
     week_start = int(time.mktime((struct_now.tm_year, struct_now.tm_mon, struct_now.tm_mday, 0, 0, 0, 0, 0, 0))) - 86400 * (1+struct_now.tm_wday)
     # print month_start, week_start
-    period_list = [{'name': 'all', 'title': '总排行', 'time': 0},
-                   {'name': 'semester', 'title': '学期排行', 'time': config.SEMESTER_BASE},
-                   {'name': 'month', 'title': '月排行', 'time': month_start},
-                   {'name': 'week', 'title': '周排行', 'time': week_start}]
+    period_list = [{'name': 'all', 'title': '总排行', 'time': 0, 'notice': u'本系统所有值班时间之和'},
+                   {'name': 'semester', 'title': '学期排行', 'time': config.SEMESTER_BASE, 'notice': u'本学期值班时间之和间'},
+                   {'name': 'month', 'title': '月排行', 'time': month_start, 'notice': u'自本月1日以来值班时间之和'},
+                   {'name': 'week', 'title': '周排行', 'time': week_start, 'notice': u'自本周日以来值班时间之和'}]
     sqltpl = "select c.uid, c.name, sum(b.end_time-b.work_start_time) as totaltime from duty as a left join activity as b on a.aid = b.id left join member as c on c.uid = a.uid where b.end_time != '0' and b.start_time > '%d' and b.end_time < %d and a.status = '11' group by a.uid order by totaltime desc limit %s "
     for period in period_list:
         sql = sqltpl % (period['time'], int(time.time()), 15)
