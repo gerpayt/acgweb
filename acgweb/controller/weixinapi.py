07e4c2e0d66b1ba68534745701637d820b34346a
陈丰 <gerpayt@qq.com>
2015-04-26 18:47:47 +0800

    API更新

--- acgweb/controller/weixinapi.py
+++ acgweb/controller/weixinapi.py
@@ -4,25 +4,44 @@ from flask import render_template, flash, jsonify, abort, make_response, json, r
 from acgweb import app, db, config
 from acgweb import config
 import hashlib
+import xml.etree.ElementTree
 
 
-@app.route('/weixinapi/token')
-def check_signature():
-    signature = request.args.get('signature')
-    echostr = request.args.get('echostr')
+@app.route('/weixinapi', methods=['GET', 'POST'])
+def weixinapi():
+    if request.method == 'GET' and request.args.get('echostr'):
+        signature = request.args.get('signature')
+        echostr = request.args.get('echostr')
 
-    timestamp = request.args.get('timestamp')
-    nonce = request.args.get('nonce')
-    token = config.WEIXIN_TOKEN
+        timestamp = request.args.get('timestamp')
+        nonce = request.args.get('nonce')
+        token = config.WEIXIN_TOKEN
 
-    sig_arr = [token, timestamp, nonce]
-    sig_arr.sort()
-    sig_str = ''.join(sig_arr)
+        sig_arr = [token, timestamp, nonce]
+        sig_arr.sort()
+        sig_str = ''.join(sig_arr)
 
-    sha1 = hashlib.sha1()
-    sha1.update(sig_str)
+        sha1 = hashlib.sha1()
+        sha1.update(sig_str)
 
-    if sha1.hexdigest() == signature:
-        return make_response(echostr)
+        if sha1.hexdigest() == signature:
+            return make_response(echostr)
+        else:
+            abort(403)
     else:
-        abort(403)
+        data = request.args.post
+        tree = xml.etree.ElementTree.fromstring(data)
+        root = tree.getroot()
+        MsgType = root.find('MsgType').text
+
+        ToUserName = root.find('ToUserName').text
+        FromUserName = root.find('FromUserName').text
+        FromUserName = root.find('FromUserName').text
+        MsgType = root.find('MsgType').text
+        MsgId = root.find('MsgId').text
+
+        tfp = open(config.BASE_DIR + 'log/weixin-'+str(MsgId)+'.log', 'w')
+        tfp.write(data)
+        tfp.close()
+
+        return make_response(data)
